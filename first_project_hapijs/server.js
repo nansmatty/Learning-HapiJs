53d76ffb3012c4d9a3055457c3c4df33e98445f5
Narayan Maity <narayanmaitysp1997@gmail.com>
2024-10-05 00:20:14 +0530

    different routing

--- first_project_hapijs/server.js
+++ first_project_hapijs/server.js
@@ -12,7 +12,54 @@ const init = async () => {
 		method: "GET",
 		path: "/",
 		handler: (request, h) => {
-			return "Hello World!";
+			return "<h1>Hello World!</h1>";
+		},
+	});
+
+	server.route({
+		method: "GET",
+		path: "/users/{user}",
+		handler: (req, res) => {
+			return `<h1>Hello ${req.params.user}</h1>`;
+		},
+	});
+
+	//By adding question mark in parameter it became not mandantory parameter that means without passing the parameter the route still work
+	server.route({
+		method: "GET",
+		path: "/not_madantory/{user?}",
+		handler: (req, res) => {
+			if (req.params.user) {
+				return `<h1>Hello ${req.params.user}</h1>`;
+			}
+			return `<h1>Hello Stranger</h1>`;
+		},
+	});
+
+	// Query parameters
+	server.route({
+		method: "GET",
+		path: "/query_parameter",
+		handler: (req, res) => {
+			return `<h1>Hello ${req.query.name} ${req.query.lastname ? req.query.lastname : ""}</h1>`;
+		},
+	});
+
+	//redirections
+	server.route({
+		method: "GET",
+		path: "/redirection",
+		handler: (req, res) => {
+			return res.redirect("/");
+		},
+	});
+
+	//Handling 404 pages
+	server.route({
+		method: "GET",
+		path: "/{any*}",
+		handler: (req, res) => {
+			return "<h1>Oh no! You must be lost!</h1>";
 		},
 	});
 
